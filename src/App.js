e24b02c00bfb5d1e628a95f2a14a5e97f355a48c
Shengyu Wang <shengyuwang95@gmail.com>
2021-01-12 21:39:01 +1100

    Static boilderplate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+import React, { useState } from 'react'
+import FlashCardList from './FlashCardList';
+
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
   return (
-    <h1>Hello World!</h1>
+    <FlashCardList flashcards={flashcards} />
   );
 }
 
 export default App;
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'Question 1',
+    answer: 'Option A',
+    options: [
+      'A',
+      'B',
+      'C',
+      'D'
+    ]
+  },
+  {
+    id: 2,
+    question: 'Question 2',
+    answer: 'Option E',
+    options: [
+      'E',
+      'F',
+      'G',
+      'H'
+    ]
+  },
+  {
+    id: 3,
+    question: 'Question 3',
+    answer: 'Option I',
+    options: [
+      'I',
+      'L',
+      'M',
+      'N'
+    ]
+  }
+]
