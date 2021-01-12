0c1ed7dd5dd36b05db5b3d0b1fa741cb353537cb
Shengyu Wang <shengyuwang95@gmail.com>
2021-01-13 08:01:15 +1100

    Add API calls and decoding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import FlashCardList from './FlashCardList';
 import './app.css'
+import axios from 'axios'
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+
+  useEffect(() => {
+    axios
+      .get('https://opentdb.com/api.php?amount=10')
+      .then(res => {
+        setFlashcards(
+          res.data.results.map((questionItem, index) => {
+            const answer = decodeString(questionItem.correct_answer)
+            const options = [...questionItem.incorrect_answers.map(a => decodeString(a)),
+              answer]
+            return {
+              id: `${index}-$(Data.now)`,
+              question: decodeString(questionItem.question),
+              answer: questionItem.correct_answer,
+              options: options.sort(() => Math.random() - .5)
+            }
+          })
+        )
+      })
+  }, [])
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
   return (
     <FlashCardList flashcards={flashcards} />
   );
